7890b917e59317220d9353c46eaa48f5bfbf01ae
cat-or-fish <viselle@web.de>
2025-08-12 15:34:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ def load_css(path):
     with open(path, "r") as f:
         css = f.read()
     st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
-load_css("./design.css")
+css_path = "./design.css"
+load_css(css_path)
 
 st.write("Das ist mein Lernprojekt")
 
